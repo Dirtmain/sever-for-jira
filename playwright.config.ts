d8fd0b307ed88a390112291923d6535212b5cec8
Dirtmain <szymonn760@gmail.com>
2025-12-15 09:23:47 +0100

    more slop

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['junit', { outputFile: 'test-results/results.xml' }],
-]
+],
 
 
 
